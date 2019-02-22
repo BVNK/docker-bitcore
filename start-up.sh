7b8483044ef1d0db40217bb6c788602effc35fed
Philip Wood <philip.wood@gmail.com>
2019-02-22 13:24:32 +0200

    start up shell script used to copy config files to PVC location

---
+++ start-up.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#
+# The intent of this start-up script is to copy bitcore config files to the PVC location 
+# `/root/.bitcore/data` in the Helm Chart [crypto-testnet](https://github.com/BVNK/helm-charts/tree/1-create-helm-chart-for-bitcoin-testnet/crypto/bitcoin-testnet)
+# to overcome configmap/PVC mapping issues.
+cp /root/.bitcore/config/* /root/.bitcore/data
+bitcored
